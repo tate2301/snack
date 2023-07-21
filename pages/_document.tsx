66eaf2628a37187cf990a0f80f4b20bf28cb0c06
Kamfeskaya <tatendachris@gmail.com>
2023-07-21 23:49:24 +0200

    ss

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,15 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/thumbnail." />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
           />
         </Head>
         <body>
