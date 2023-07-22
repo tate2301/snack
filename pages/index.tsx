3185a8b4fa680e795cfad4eb3a502cc0a82b90fc
Kamfeskaya <tatendachris@gmail.com>
2023-07-22 02:24:17 +0200

    add dates

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,16 @@ export default function Page() {
   return (
     <main className="h-full w-full flex flex-col justify-between">
       <div className="flex-1 overflow-y-auto">
+        <div className="flex gap-2 items-center">
+          <div className="p-4 py-2 flex items-center border flex-col gap-1 rounded-xl text-centers">
+            <p className="text-xl font-bold">21</p>
+            <p className="text-sm">Fri</p>
+          </div>
+          <div className="p-4 py-2 shadow flex items-center flex-col gap-1 rounded-xl text-center bg-zinc-950 text-white">
+            <p className="text-xl font-bold">22</p>
+            <p className="text-sm">Today</p>
+          </div>
+        </div>
         <div className="p-4">
           <motion.div layout className="flex flex-col mt-2">
             <TaskItem id={1} />
