4703bdf35e98465440104a868d45ea976093c352
Kamfeskaya <tatendachris@gmail.com>
2023-07-22 02:28:08 +0200

    use add utem

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,15 +22,33 @@ export default function Page() {
   return (
     <main className="h-full w-full flex flex-col justify-between">
       <div className="flex-1 overflow-y-auto">
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center p-4">
           <div className="p-4 py-2 flex items-center border flex-col gap-1 rounded-xl text-centers">
-            <p className="text-xl font-bold">21</p>
-            <p className="text-sm">Fri</p>
+            <p className="text-xl font-bold">19</p>
+            <p className="text-sm">Mon</p>
+          </div>
+          <div className="p-4 py-2 flex items-center border flex-col gap-1 rounded-xl text-centers">
+            <p className="text-xl font-bold">20</p>
+            <p className="text-sm">Tue</p>
           </div>
           <div className="p-4 py-2 shadow flex items-center flex-col gap-1 rounded-xl text-center bg-zinc-950 text-white">
-            <p className="text-xl font-bold">22</p>
-            <p className="text-sm">Today</p>
+            <p className="text-xl font-bold">21</p>
+            <p className="text-sm">Wed</p>
           </div>
+          <div className="p-4 py-2 flex items-center border flex-col gap-1 rounded-xl text-centers">
+            <p className="text-xl font-bold">22</p>
+            <p className="text-sm">Thu</p>
+          </div>
+          <div className="p-4 py-2 flex items-center border flex-col gap-1 rounded-xl text-centers">
+            <p className="text-xl font-bold">23</p>
+            <p className="text-sm">Fri</p>
+          </div>
+        </div>
+        <div className="px-4">
+          <button className="flex gap-4 items-center text-blue-600 font-bold">
+            <PlusIcon className="w-4 h-4" />
+            <p>Add New Task</p>
+          </button>
         </div>
         <div className="p-4">
           <motion.div layout className="flex flex-col mt-2">
