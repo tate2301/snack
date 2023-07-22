9b68c36acb6d8c29a00e3b151274903ea89b030d
Kamfeskaya <tatendachris@gmail.com>
2023-07-22 11:17:25 +0200

    add padding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import "../styles/global.css";
 export default function App({ Component, pageProps }) {
   return (
     <div
-      className="container bg-white rounded-xl h-screen mx-auto  text-zinc-800 overflow-hidden subpixel-antialiased text-base"
+      className="container p-2 bg-white rounded-xl h-screen mx-auto  text-zinc-800 overflow-hidden subpixel-antialiased text-base"
       id="app-container"
     >
       <Component {...pageProps} />
