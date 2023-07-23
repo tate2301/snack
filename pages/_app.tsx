a36daa1c102022742d9777c8b2aeed3842e4b3ea
Kamfeskaya <tatendachris@gmail.com>
2023-07-23 04:03:31 +0200

    fix: remove dev preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import '../styles/global.css';
-import { DevSupport } from '@react-buddy/ide-toolbox';
-import { ComponentPreviews, useInitial } from '../dev';
 
 export default function App({ Component, pageProps }) {
 	return (
@@ -8,11 +6,8 @@ export default function App({ Component, pageProps }) {
 			className='container p-2 bg-white rounded-xl h-screen mx-auto  overflow-y-auto   text-zinc-800 overflow-hidden subpixel-antialiased text-base'
 			id='app-container'
 		>
-			<DevSupport ComponentPreviews={ComponentPreviews}
-									useInitialHook={useInitial}
-			>
-				<Component {...pageProps} />
-			</DevSupport>
+
+			<Component {...pageProps} />
 		</div>
 	);
 }
